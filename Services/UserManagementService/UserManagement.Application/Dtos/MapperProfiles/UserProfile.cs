3be9a60e0a7a94803c111175aa1264e6f4bc1933
Stenio Joseph <steniojoseph@hotmail.com>
2024-08-25 23:13:46 -0400

    Feat: - Add User.cs: represents the user entity with properties: Id, Username, Password, Email - Added Dtos for data transfer objects   - UserDto.cs: Data Transfer Object for user-related data   - UserProfile.cs: AutoMapper profile for mapping user entity to UserDto

---
+++ Services/UserManagementService/UserManagement.Application/Dtos/MapperProfiles/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Application.Dtos.MapperProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDto>();
+        }
+    }
+}
---
+++ Services/UserManagementService/UserManagement.Application/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace UserManagement.Application.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
