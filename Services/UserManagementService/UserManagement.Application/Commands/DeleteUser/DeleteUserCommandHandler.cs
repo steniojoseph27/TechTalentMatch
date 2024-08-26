fecaa0ff06f348ff38f307bc95ee4b744e9046ec
Stenio Joseph <steniojoseph@hotmail.com>
2024-08-26 02:06:46 -0400

    - DeleteUserCommand.cs: Command for deleting an existing user   - DeleteUserCommandHandler.cs: Handles the logic for deleting an existing user

---
+++ Services/UserManagementService/UserManagement.Application/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using UserManagement.Domain.Exceptions;
+using UserManagement.Infrastructure.Data.Repositories;
+
+namespace UserManagement.Application.Commands.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByUserIdAsync(request.Id);
+            if (user == null)
+            {
+                throw new UserNotFoundException<int>(request.Id);
+            }
+
+            await _userRepository.DeleteUserAsync(request.Id);
+            return Unit.Value;
+        }
+    }
+}
