0d54e9dbe2b22fd7d0be231831c687dde9468ef9
Stenio Joseph <steniojoseph@hotmail.com>
2024-08-26 02:12:55 -0400

    Feat: - Add Startup.cs for configuring services

--- Services/UserManagementService/UserManagement.API/Startup.cs
+++ Services/UserManagementService/UserManagement.API/Startup.cs
@@ -1,6 +1,25 @@
-﻿namespace UserManagement.API
+﻿using UserManagement.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagement.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UserManagementDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
     }
 }
