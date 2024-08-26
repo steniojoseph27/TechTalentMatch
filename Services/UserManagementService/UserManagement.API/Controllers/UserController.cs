69b8474b7a76d687af67c9fc600c2db6125767aa
Stenio Joseph <steniojoseph@hotmail.com>
2024-08-26 15:59:43 -0400

    Feat: - Added Controllers for handling API requests   - UserController.cs: Handles user-related operations (CRUD operations, profile management) - Add UserDto.cs: Data Transfer Object for user-related data, including properties: Id, Username, Email - Refactor Startup.cs for configuring middleware - Refactor UserManagement.API.csproj add MediatR

---
+++ Services/UserManagementService/UserManagement.API/Controllers/UserController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.Domain.Exceptions;
+using MediatR;
+using UserManagement.API.Models;
+using UserManagement.Application.Commands.CreateUser;
+using UserManagement.Application.Queries.GetAllUsers;
+using UserManagement.Application.Queries.GetUserById;
+using UserManagement.Application.Queries.GetUserByUserName;
+using UserManagement.Application.Commands.UpdateUser;
+using UserManagement.Application.Commands.DeleteUser;
+
+namespace UserManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createdUser = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetUserById), new { id = createdUser.Id }, createdUser);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserDto>>> GetAllUsers()
+        {
+            var query = new GetAllUsersQuery();
+            var users = await _mediator.Send(query);
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetUserById(int id)
+        {
+            try
+            {
+                var query = new GetUserByIdQuery { Id = id };
+                var user = await _mediator.Send(query);
+                return Ok(user);
+            }
+            catch (UserNotFoundException<int>)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("username/{username}")]
+        public async Task<ActionResult<UserDto>> GetUserByUsername(string username)
+        {
+            try
+            {
+                var query = new GetUserByUserNameQuery { Username = username };
+                var user = await _mediator.Send(query);
+                return Ok(user);
+            }
+            catch (UserNotFoundException<string>)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] UpdateUserCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest("User ID mismatch");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _mediator.Send(command);
+                return NoContent();
+            }
+            catch (UserNotFoundException<int>)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            try
+            {
+                var command = new DeleteUserCommand { Id = id };
+                await _mediator.Send(command);
+                return NoContent();
+            }
+            catch (UserNotFoundException<int>)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- Services/UserManagementService/UserManagement.API/Startup.cs
+++ Services/UserManagementService/UserManagement.API/Startup.cs
@@ -1,6 +1,12 @@
 ﻿using UserManagement.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using UserManagement.Application.Dtos.MapperProfiles;
+using UserManagement.Infrastructure.Data.Repositories;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Text.Json;
+using System.Reflection;
+using UserManagement.Application.Commands.CreateUser;
 
 namespace UserManagement.API
 {
@@ -15,11 +21,60 @@ namespace UserManagement.API
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<UserManagementDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
+
+            services.AddDbContext<UserManagementDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(UserProfile));
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
+                Assembly.GetExecutingAssembly(),
+                typeof(CreateUserCommandHandler).Assembly));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserManagement.API v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler(exceptionHandlerApp =>
+                {
+                    exceptionHandlerApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                        context.Response.ContentType = "application/json";
+                        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                        var exception = exceptionHandlerPathFeature?.Error;
+
+                        var problemDetails = new ProblemDetails
+                        {
+                            Status = StatusCodes.Status500InternalServerError,
+                            Title = "An error occurred while processing your request.",
+                            Detail = exception?.Message
+                        };
+
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails));
+                    });
+                });
+
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
