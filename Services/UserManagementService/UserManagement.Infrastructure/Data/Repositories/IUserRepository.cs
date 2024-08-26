6269c37c5927463e5fe4bd699840e25b9dc225ca
Stenio Joseph <steniojoseph@hotmail.com>
2024-08-25 23:26:57 -0400

    Feat: - Add UserNotFoundException.cs: Custom exception thrown when a user is not found. - Add UserManagementDbContext.cs: Entity Framework DbContext for managing user-related database operations   - UserRepository.cs: Implements IUserRepository for CRUD operations on user entities   - IUserRepository.cs: Interface defining repository methods for user entities

---
+++ Services/UserManagementService/UserManagement.Infrastructure/Data/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Infrastructure.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> AddUserAsync(User user);
+        Task<List<User>> GetAllUsersAsync();
+        Task<User> GetByUserIdAsync(int userId);
+        Task<User> GetByUserNameAsync(string userName);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int userId);
+    }
+}
