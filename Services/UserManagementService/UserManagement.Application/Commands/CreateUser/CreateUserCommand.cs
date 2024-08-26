7a56c8ce452afac33975589af89ec56fd900382a
Stenio Joseph <steniojoseph@hotmail.com>
2024-08-26 01:19:56 -0400

    Feat: Added Commands for user creation   - CreateUserCommand.cs: Command for creating a new user   - CreateUserCommandHandler.cs: Handles the logic for creating a new user   - CreateUserCommandValidator.cs: Validates the data for creating a new user

---
+++ Services/UserManagementService/UserManagement.Application/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Application.Commands.CreateUser
+{
+    public class CreateUserCommand : IRequest<User>
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
