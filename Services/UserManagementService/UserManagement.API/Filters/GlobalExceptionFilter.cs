ff04ce82221ce5b1ef9f3c2f5032a910f2ffb34d
Stenio Joseph <steniojoseph@hotmail.com>
2024-08-28 12:52:28 -0400

    - Add GlobalExceptionFilter.cs: Catches and handles global exceptions, providing consistent error responses   - Add ValidationFilter.cs: Validates incoming requests and ensures data integrity - Add ValidationException.cs: Custom exception thrown when validation fails

---
+++ Services/UserManagementService/UserManagement.API/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace UserManagement.API.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+            var response = new
+            {
+                Message = "An unexpected error occurred.",
+                Details = exception.Message
+            };
+
+            context.Result = new JsonResult(response)
+            {
+                StatusCode = (int)HttpStatusCode.InternalServerError
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ Services/UserManagementService/UserManagement.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,8 @@
+﻿
+namespace UserManagement.Domain.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message) { }
+    }
+}
