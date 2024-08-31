d917467036e3c4d168956ffc68823ccc971597c0
steniojoseph27 <steniojoseph@hotmail.com>
2024-08-30 23:30:05 -0400

    Feat: - Add Entities - Curriculum.cs: Domain entity for the curriculum. - JobDescription.cs: Domain entity for the job description. - CandidateProfile.cs: Domain entity for the candidate profile.

---
+++ Services/CurriculumBuilderService/CurriculumBuilderService.Domain/Entities/Curriculum.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CurriculumBuilderService.Domain.Entities
+{
+    public class Curriculum
+    {
+        public int Id { get; set; }
+        public string ProfessionalHeader { get; set; } = string.Empty;
+        public string ProfessionalSummary { get; set; } = string.Empty;
+        public string Education { get; set; } = string.Empty;
+        public string WorkExperience { get; set; } = string.Empty;
+        public List<string>? Skills { get; set; }
+        public List<string>? Certifications { get; set; }
+        public List<string>? AdditionalAccomplishments { get; set; }
+        public string Language { get; set; } = string.Empty;
+    }
+}
---
+++ Services/CurriculumBuilderService/CurriculumBuilderService.Domain/Entities/JobDescription.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CurriculumBuilderService.Domain.Entities
+{
+    public class JobDescription
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public string Responsibilities { get; set; } = string.Empty;
+        public List<string>? RequiredSkills { get; set; }
+        public List<string>? PreferredSkills { get; set; }
+        public string Location { get; set; } = string.Empty;
+    }
+}
