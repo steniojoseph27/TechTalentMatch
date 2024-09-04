667984b09e3d8eb23a4ebba5e09bf8b80d645f34
steniojoseph27 <steniojoseph@hotmail.com>
2024-09-04 01:14:19 -0400

    Feat: - Skill.cs: Represents a skill in both the job description and the candidate profile. - Qualification.cs: Represents a qualification required by the job or possessed by the candidate. - Experience.cs: Represents work experience.

---
+++ Services/CurriculumBuilderService/CurriculumBuilderService.Domain/ValueObjects/Skill.cs
@@ -0,0 +1,13 @@
+﻿using CurriculumBuilderService.Domain.Entities;
+
+namespace CurriculumBuilderService.Domain.ValueObjects
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsRequired { get; set; }
+        public int JobDescriptionId { get; set; }
+        public JobDescription? JobDescription { get; set; }
+    }
+}
