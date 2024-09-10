e797e6e81b8d76d5b5f550f431a94fd5342f8d8c
steniojoseph27 <steniojoseph@hotmail.com>
2024-09-09 22:43:12 -0400

    Feat: - ICurriculumServiceBuilder.cs: interface for curriculum service builder. - CurriculumServiceBuilder.cs: Implement curriculum creation business logic. - CurriculumController.cs: Handle curriculum creation.

---
+++ Services/CurriculumBuilderService/CurriculumBuilderService.Application/Interfaces/ICurriculumServiceBuilder.cs
@@ -0,0 +1,9 @@
+﻿using CurriculumBuilderService.Application.DTOs;
+
+namespace CurriculumBuilderService.Application.Interfaces
+{
+    public interface ICurriculumServiceBuilder
+    {
+        Task<CurriculumDto> BuildCurriculumAsync(CandidateProfileDto candidateProfile, JobDescriptionDto jobDescription);
+    }
+}
