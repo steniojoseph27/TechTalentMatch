08bfb7f71f13b1a9cb17c1a66f889d3b85b6951d
steniojoseph27 <steniojoseph@yahoo.com>
2024-09-29 02:32:18 -0400

    Feat: - Add Startup.cs for configuring services and middleware - Add Program.cs as the entry point of the application - Add ResumeUploadDto.cs data transfer object for resume upload - Add ResumeParsingController.cs to manage resume parsing

---
+++ Services/ResumeParsingService/ResumeParsingService.API/Controllers/ResumeParsingController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ResumeParsingService.API.DTOs;
+using ResumeParsingService.Application.Commands;
+
+namespace ResumeParsingService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ResumeParsingController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ResumeParsingController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadResume([FromBody] ResumeUploadDto resumeUploadDto)
+        {
+            var command = new ParseResumeCommand(resumeUploadDto.ResumeData, resumeUploadDto.FileType);
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
---
+++ Services/ResumeParsingService/ResumeParsingService.API/Program.cs
@@ -0,0 +1,21 @@
+﻿using UserManagement.API;
+
+namespace ResumeParsingService.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
+    }
+}
