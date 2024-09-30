789f444129c5878936a928bc86cc46a57edbcfc9
steniojoseph27 <steniojoseph@yahoo.com>
2024-09-29 22:09:49 -0400

    Feat: - Add FileType enum for the file types option - ParseResumeCommand.cs: Represents the command to parse a resume - IResumeParser.cs: Defines the contract for a resume parser service - Add IResumeRepository.cs: Defines the contract for a resume repository

---
+++ Services/ResumeParsingService/ResumeParsingService.Application/Interfaces/IResumeRepository.cs
@@ -0,0 +1,10 @@
+﻿using ResumeParsingService.Application.Models;
+
+namespace ResumeParsingService.Application.Interfaces
+{
+    public interface IResumeRepository
+    {
+        Task SaveResume(Resume resume);
+        Task<Resume> GetResumeById(Guid id);
+    }
+}
