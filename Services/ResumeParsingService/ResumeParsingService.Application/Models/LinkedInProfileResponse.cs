a76b1be2ed5d48f4a4f0f10a4ad8ac16d89a921d
steniojoseph27 <steniojoseph@yahoo.com>
2024-10-17 00:35:11 -0400

    Feat: - Add LinkedInProfileResponse

---
+++ Services/ResumeParsingService/ResumeParsingService.Application/Models/LinkedInProfileResponse.cs
@@ -0,0 +1,10 @@
+
+namespace ResumeParsingService.Application.DTOs
+{
+    public class LinkedInProfileResponse
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Headline { get; set; } = string.Empty;
+    }
+}
